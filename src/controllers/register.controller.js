3606a76b790b4617808c250da5db932f71e65882
gpaul <40648068+gpaul-faldin@users.noreply.github.com>
2024-02-08 16:17:29 +0100

    Fix variable names in schema

--- src/controllers/register.controller.js
+++ src/controllers/register.controller.js
@@ -14,12 +14,20 @@ async function hashPassword(password) {
 }
 
 exports.Register = async (req, res, next) => {
-    const { name, surname, mail, password, adress, postalCode, town, phone } =
-        req.body;
+    const {
+        lastname,
+        firstname,
+        mail,
+        password,
+        adress,
+        postalCode,
+        town,
+        phone,
+    } = req.body;
 
     if (
-        !name ||
-        !surname ||
+        !firstname ||
+        !lastname ||
         !mail ||
         !password ||
         !adress ||
@@ -47,8 +55,8 @@ exports.Register = async (req, res, next) => {
         }
 
         const newUser = await User.create({
-            name,
-            surname,
+            lastname,
+            firstname,
             mail,
             password: await hashPassword(password),
             adress,
