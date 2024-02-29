df0d79bf3bee1babb0858245ce8baf6702f487f0
KGROLLEMUND <kyllian.grollemund@ynov.com>
2024-02-29 11:24:43 +0100

    add checkjwt in route

--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -1,11 +1,12 @@
 const express = require("express");
+const checkJWT = require("../middlewares/checkJWT")
 
 const router = express.Router();
 const productController = require("../controllers/product.controller");
 
-router.get("/:id", productController.getProduct);
-router.get("/", productController.getProducts);
-router.delete("/delete/:id", productController.deleteProduct);
-router.put("/update/:id", productController.updateProduct);
+router.get("/:id", checkJWT, productController.getProduct);
+router.get("/", checkJWT, productController.getProducts);
+router.delete("/delete/:id", checkJWT, productController.deleteProduct);
+router.put("/update/:id", checkJWT, productController.updateProduct);
 
 module.exports = router;
--- src/routes/whishlist.route.js
+++ src/routes/whishlist.route.js
@@ -1,11 +1,12 @@
 const express = require("express");
+const checkJWT = require("../middlewares/checkJWT")
 
 const router = express.Router();
 const whishlistController = require('../controllers/whishlist.controller');
 
-router.get('/:id', whishlistController.getWhishlist); 
-router.get('/', whishlistController.getWhishlists);
-router.post('/', whishlistController.addWhishlist);
-router.delete('/delete/:id', whishlistController.deleteWhishlist);
+router.get('/:id', checkJWT, whishlistController.getWhishlist); 
+router.get('/', checkJWT, whishlistController.getWhishlists);
+router.post('/', checkJWT, whishlistController.addWhishlist);
+router.delete('/delete/:id', checkJWT, whishlistController.deleteWhishlist);
 
 module.exports = router;
