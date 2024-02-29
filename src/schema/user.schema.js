508073b0a2097bb2b1adb1785f3adcc3d81e0dc4
gpaul <40648068+gpaul-faldin@users.noreply.github.com>
2024-02-29 16:10:32 +0100

    added swagger on /api-docs

--- src/schema/user.schema.js
+++ src/schema/user.schema.js
@@ -1,14 +1,14 @@
 const mongoose = require("mongoose");
 
 var userSchema = new mongoose.Schema({
-    firstname: { type: String, required: true },
-    lastname: { type: String, required: true },
-    mail: { type: String, required: true, unique: true },
+    name: { type: String, required: true },
+    surname: { type: String, required: true },
+    mail: { type: String, required: true },
     password: { type: String, required: true },
     adress: { type: String, required: true },
     postalCode: { type: Number, required: true },
     town: { type: String, required: true },
-    phone: { type: String, required: true, unique: true },
+    phone: { type: String, required: true },
     admin: { type: Boolean, default: false },
     wishlist: [
         {
