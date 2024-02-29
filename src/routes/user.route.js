0e337cce3f06bcc65c92ffd2f2bf68f46ae91230
JSFlobert170 <flobertjohnson@gmail.com>
2024-02-29 12:36:49 +0100

    Added jwt token

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,11 +1,11 @@
-const express = require("express");
-
+const express = require('express');
+const checkJWT = require('../middlewares/checkJWT')
 const router = express.Router();
 const userController = require('../controllers/user.controller');
 
-router.get('/:id', userController.getUser);
-router.get('/', userController.getUsers);
-router.delete('/delete/:id', userController.deleteUser);
-router.put('/update/:id', userController.updateUser);
+router.get('/:id', checkJWT, userController.getUser);
+router.get('/', checkJWT, userController.getUsers);
+router.delete('/delete/:id', checkJWT, userController.deleteUser);
+router.put('/update/:id', checkJWT, userController.updateUser);
 
 module.exports = router;
