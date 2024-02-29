3f6d23841dc00ed285962246e9656383a349b9e1
gpaul <40648068+gpaul-faldin@users.noreply.github.com>
2024-02-29 13:10:12 +0100

    fix

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,12 @@ const express = require("express");
 const router = express.Router();
 const productRoute = require("./product.route");
 const authRoute = require("./auth.route");
+const userRoute = require("./user.route");
 const wishlistRoute = require("./whishlist.route");
 
 router.use("/products", productRoute);
 router.use("/auth", authRoute);
 router.use("/whishlist", wishlistRoute);
-router.use('/users', userRoute);
+router.use("/users", userRoute);
 
 module.exports = router;
