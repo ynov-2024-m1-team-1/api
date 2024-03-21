5a412085bf1dd9c7643ba8b08493d1daaf9032bc
gpaul <40648068+gpaul-faldin@users.noreply.github.com>
2024-03-21 15:05:47 +0100

    update mail to email login

--- src/controllers/login.controller.js
+++ src/controllers/login.controller.js
@@ -3,34 +3,39 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 exports.Login = async (req, res, next) => {
-    const { mail, password } = req.body;
-    if (!mail || !password) {
-        return res.send({ 
-            message: "Missing email or password", 
-            code: 400 });
+    const { email, password } = req.body;
+    if (!email || !password) {
+        return res.send({
+            message: "Missing email or password",
+            code: 400,
+        });
     }
     try {
-        const user = await User.findOne({ mail });
+        const user = await User.findOne({ email });
         if (!user) {
-            return res.send({ 
-            message: "User not found", 
-            code: 404 });
+            return res.send({
+                message: "User not found",
+                code: 404,
+            });
         }
         const passwordMatch = await bcrypt.compare(password, user.password);
         if (!passwordMatch) {
             return res.send({ message: "Incorrect password", code: 401 });
         }
-        const token = jwt.sign({ 
-            id: user._id,
-            admin: user.admin
-            }, 
-            process.env.JWT_SECRET, {expiresIn: "365d"})
-        
-        return res.send({ 
-            message: "Login successful", 
+        const token = jwt.sign(
+            {
+                id: user._id,
+                admin: user.admin,
+            },
+            process.env.JWT_SECRET,
+            { expiresIn: "365d" }
+        );
+
+        return res.send({
+            message: "Login successful",
             code: 200,
-            data: token
-            });
+            data: token,
+        });
     } catch (error) {
         console.error(error);
         return res.send({ message: error.message, code: 500 });
