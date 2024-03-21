e9a3bfd5d112386b25dff4382bcefc5e547421a2
gpaul <40648068+gpaul-faldin@users.noreply.github.com>
2024-03-21 13:06:15 +0100

    getme route

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,11 +1,11 @@
-const user = require('../schema/user.schema')
+const user = require("../schema/user.schema");
 
-exports.getUsers = async(req, res, next)=>{
-    if(!req.userToken.admin){
+exports.getUsers = async (req, res, next) => {
+    if (!req.userToken.admin) {
         return res.json({
             code: 401,
             message: "Admin access required",
-        })
+        });
     }
     try {
         const users = await user.find();
@@ -13,66 +13,87 @@ exports.getUsers = async(req, res, next)=>{
             return res.json({
                 code: 404,
                 message: "User not found",
-            })
+            });
         }
-        return res.json(
-            {
-                message: "Success",
-                code: 200,
-                data: users,
-            },
-        );
-
-    } catch(err) {
+        return res.json({
+            message: "Success",
+            code: 200,
+            data: users,
+        });
+    } catch (err) {
         console.log(`erreur : ${err}`);
-        return res.json(
-            {
-                message: "Failed to retrieves users",
-                code: 400,
-            },
-        );
+        return res.json({
+            message: "Failed to retrieves users",
+            code: 400,
+        });
     }
-
-}
+};
 
 exports.getUser = async (req, res, next) => {
-
     try {
         const id = req.params.id;
         if (!id) {
             return res.json({
                 code: 400,
                 message: "Id is required",
-            })
+            });
         }
-        if(id !== req.userToken.id || req.userToken.admin != true){
+        if (id !== req.userToken.id || req.userToken.admin != true) {
             return res.json({
                 code: 401,
                 message: "Unauthorized",
-            })
+            });
         }
         const users = await user.findById(String(id));
         if (!users) {
             return res.json({
                 code: 404,
                 message: "User not found",
-            })
+            });
         }
 
-            return res.json({
-                message: "Success",
-                code: 200,
-                data: users,
-            });
-        
+        return res.json({
+            message: "Success",
+            code: 200,
+            data: users,
+        });
     } catch (err) {
         console.log(`erreur : ${err}`);
-        return res.json(
-            {
-                message: "bad request",
+        return res.json({
+            message: "bad request",
+            code: 400,
+        });
+    }
+};
+
+exports.getMe = async (req, res, next) => {
+    try {
+        const id = req.userToken.id;
+        if (!id) {
+            return res.json({
                 code: 400,
-            },
-        );
+                message: "Id is required",
+            });
+        }
+        const users = await user.findById(String(id));
+        if (!users) {
+            return res.json({
+                code: 404,
+                message: "User not found",
+            });
+        }
+
+        return res.json({
+            message: "Success",
+            code: 200,
+            data: users,
+        });
+    } catch (err) {
+        console.log(`erreur : ${err}`);
+        return res.json({
+            message: "bad request",
+            code: 400,
+        });
     }
 };
 
@@ -83,36 +104,32 @@ exports.deleteUser = async (req, res, next) => {
             return res.json({
                 code: 400,
                 message: "Id is required",
-            })
+            });
         }
-        if(id !== req.userToken.id || req.userToken.admin != true){
+        if (id !== req.userToken.id || req.userToken.admin != true) {
             return res.json({
                 code: 401,
                 message: "Unauthorized",
-            })
+            });
         }
         const users = await user.deleteOne({ _id: id });
         if (!users) {
             return res.json({
                 code: 404,
                 message: "User not found",
-            })
+            });
         }
-        return res.json(
-            {
-                message: "Success",
-                code: 200,
-                data: users,
-            },
-        );
+        return res.json({
+            message: "Success",
+            code: 200,
+            data: users,
+        });
     } catch (err) {
         console.log(`erreur : ${err}`);
-        return res.json(
-            {
-                message: "bad request",
-                code: 400,
-            },
-        );
+        return res.json({
+            message: "bad request",
+            code: 400,
+        });
     }
 };
 
@@ -124,34 +141,33 @@ exports.updateUser = async (req, res, next) => {
             return res.json({
                 code: 400,
                 message: "Id is required",
-            })
+            });
         }
-        if(id !== req.userToken.id || req.userToken.admin != true){
+        if (id !== req.userToken.id || req.userToken.admin != true) {
             return res.json({
                 code: 401,
                 message: "Unauthorized",
-            })
+            });
         }
-        const users = await user.findByIdAndUpdate(String(id), body, {returnDocument: 'after'});
+        const users = await user.findByIdAndUpdate(String(id), body, {
+            returnDocument: "after",
+        });
         if (!users) {
             return res.json({
                 code: 404,
                 message: "User not found",
-            })
+            });
         }
         return res.json({
             message: "Success",
             code: 200,
             data: users,
         });
-        
     } catch (err) {
         console.log(`erreur : ${err}`);
-        return res.json(
-            {
-                message: "bad request",
-                code: 400,
-            },
-        );
+        return res.json({
+            message: "bad request",
+            code: 400,
+        });
     }
 };
